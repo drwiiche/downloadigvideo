42e84abc74ef374285d4ee16b9f0afef5ac39774
BRAHIM EL-MOUDEN <drwiiche@users.noreply.github.com>
2023-09-24 20:17:13 +0100

    Update site.ts

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -4,10 +4,10 @@ export const siteConfig: SiteConfig = {
   name: "Instagram Video Downloader",
   description:
     "Unlock the power to download Instagram videos effortlessly with our Instagram Video Downloader. Get high-quality video downloads in seconds. Try it now!",
-  url: "https://download-ig-videos.vercel.app/",
-  ogImageUrl: "https://download-ig-videos.vercel.app/images/og.png",
+  url: "http://downloadvideoig.com/",
+  ogImageUrl: "http://downloadvideoig.com/images/og.png",
   links: {
-    twitter: "https://twitter.com/riadazz",
+    twitter: "https://twitter.com/badadiss",
     github: "https://github.com/riad-azz/instagram-video-downloader",
   },
 };
